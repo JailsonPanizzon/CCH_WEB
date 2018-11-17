16905896bae49f3a080e35c7e96fbed9510738cf
JailsonPanizzon <jailson.panizzon@gmail.com>
2018-11-17 12:03:16 -0200

    Efeito do painel superior

---
+++ arquivo.js
@@ -0,0 +1,11 @@
+function dizOla(){
+	document.getElementById("ola").innerHTML = "Ola " + document.getElementById("nome").value;
+}
+
+//$("button").attr("onclick","dizOla()");
+
+$( "button" ).click(function() {
+  $("#ola").text("Ola " + $("#nome").val());
+  $("#nome").attr("value","Assim também muda o texto");
+});
+  
